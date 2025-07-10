6ef57f2a028b3dc81b54b2eab6536a1046639284
Throb2003 <161972349+Throb2003@users.noreply.github.com>
2025-07-11 00:12:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("revealBtn").addEventListener("click", function () {
+  const letter = document.getElementById("letter");
+  letter.style.display = "block";
+  this.style.display = "none";
+});
