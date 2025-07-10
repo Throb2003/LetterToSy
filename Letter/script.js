a7b7eaa047fb6d33a9048e1a76496cbe4f5c7c7e
Throb2003 <161972349+Throb2003@users.noreply.github.com>
2025-07-11 00:07:41 +0300

    Delete Letter/script.js

--- Letter/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("revealBtn").addEventListener("click", function () {
-  const letter = document.getElementById("letter");
-  letter.style.display = "block";
-  this.style.display = "none";
-});
